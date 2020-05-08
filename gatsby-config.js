19cc8533da804582c1b54e54e2095749d48e2ce8
Tom Rupsis <tom@gps.lan>
2020-05-08 15:35:19 -0600

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-1648297-7`,
       },
     },
     {
